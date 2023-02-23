98da84639bcfff0119a5c97ef28f085738b985ab
Ajeibi <ajeibipikin@gmail.com>
2023-02-23 03:09:31 +0100

    https://github.com/Ajeibi

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Anim exercitation labore sint incididunt velit ad tempor pariatur amet consequat consequat duis officia. Elit officia qui laborum ipsum elit tempor fugiat ad non voluptate. Incididunt exercitation aliquip fugiat ex sit proident. Esse eu ut Lorem sint sint culpa sint est qui sint laboris proident labore voluptate. Veniam id excepteur est labore qui do irure aliqua consectetur consequat non. Exercitation laborum fugiat est qui veniam pariatur. Est non eu nulla occaecat velit reprehenderit labore deserunt labore labore ad duis.</p>
+        </div>
+    )
+} 
+
+export default About
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Anim exercitation labore sint incididunt velit ad tempor pariatur amet consequat consequat duis officia. Elit officia qui laborum ipsum elit tempor fugiat ad non voluptate. Incididunt exercitation aliquip fugiat ex sit proident. Esse eu ut Lorem sint sint culpa sint est qui sint laboris proident labore voluptate. Veniam id excepteur est labore qui do irure aliqua consectetur consequat non. Exercitation laborum fugiat est qui veniam pariatur. Est non eu nulla occaecat velit reprehenderit labore deserunt labore labore ad duis.</p>
+        </div>
+    )
+} 
+
+export default Home
